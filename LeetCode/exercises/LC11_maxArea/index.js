368a6b5b90b49719b50e09b803e85f407cf61abc
Kenny Le <kennyle37@gmail.com>
2019-09-19 13:26:53 -0700

    container with water

--- LeetCode/exercises/LC11_maxArea/index.js
+++ LeetCode/exercises/LC11_maxArea/index.js
@@ -1,3 +1,24 @@
-function maxArea(height) {}
-
+var maxArea = function(height) {
+  let largestArea = 0;
+  let left = 0;
+  let right = height.length-1;
+  let leftValue, rightValue, containerWidth, containerHeight, currentContainerSize;
+  
+  while (left < right) {
+      leftValue = height[left];
+      rightValue = height[right];
+      containerWidth = right-left;
+      containerHeight = Math.min(leftValue, rightValue);
+      currentContainerSize = containerWidth*containerHeight;
+      largestArea = Math.max(currentContainerSize, largestArea);
+      
+      if (leftValue < rightValue) {
+          left++;
+      } else {
+          right--;
+      }
+  } 
+  
+  return largestArea;
+};
 module.exports = maxArea;
