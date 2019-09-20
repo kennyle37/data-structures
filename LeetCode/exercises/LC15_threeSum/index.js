ec18ae35378bd48707bdc7ee050f5e9a6fbab217
Kenny Le <kennyle37@gmail.com>
2019-09-19 20:24:16 -0700

    maxSubArray

--- LeetCode/exercises/LC15_threeSum/index.js
+++ LeetCode/exercises/LC15_threeSum/index.js
@@ -1,3 +1,41 @@
-const threeSum = nums => {};
+var threeSum = function(nums) {
+  let res = [];
+  let left = 0;
+  let right = nums.length-1;
+  let leftValue, rightValue, current, total;
+  nums.sort((a,b) => a-b);
+  
+  for (let i = 0; i < nums.length-2; i++) {
+      if (nums[i-1] === nums[i]) continue;
+      left = i+1;
+      right = nums.length-1;
+      current = nums[i];
+
+      while (left < right) {
+          leftValue = nums[left];
+          rightValue = nums[right];
+          total = leftValue+rightValue+current;
+
+          if (left-1 !== i && nums[left-1] === leftValue) {
+              left++;
+              continue;
+          }
+          if (nums[right+1] === nums[right]) {
+              right--;
+              continue;
+          }
+          if (total === 0) {
+              res.push([current, leftValue, rightValue]);
+          } 
+          if (total <= 0) {
+              left++;
+          } else {
+              right--;
+          }
+      }
+  }
+
+  return res;
+};
 
 module.exports = threeSum;
--- LeetCode/exercises/LC53_maxSubArray/index.js
+++ LeetCode/exercises/LC53_maxSubArray/index.js
@@ -1,3 +1,16 @@
-function maxSubArray(nums) {}
+var maxSubArray = function(nums) {
+  if (nums.length === 0) return 0;
+  let dp = new Array(nums.length).fill(-Infinity);
+  dp[0] = nums[0];
+  let max = dp[0];
+
+  for (let i = 1; i < nums.length; i++) {
+      const current = nums[i];
+      dp[i] = Math.max(current, dp[i-1] + current);
+      max = Math.max(max, dp[i]);
+  }
+  return max;
+};
+
 
 module.exports = maxSubArray;
