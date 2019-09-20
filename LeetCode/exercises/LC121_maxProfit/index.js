f0125fd2a29e717e9b4390dd6f4db1288eb6bb6d
Kenny Le <kennyle37@gmail.com>
2019-09-20 09:06:22 -0700

    update

--- LeetCode/exercises/LC121_maxProfit/index.js
+++ LeetCode/exercises/LC121_maxProfit/index.js
@@ -1,3 +1,12 @@
-var maxProfit = function(prices) {};
+var maxProfit = function(prices) {
+    let result = 0;
+    let min = prices[0];
+    
+    for (let i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        result = Math.max(result, prices[i]-min);
+    }
+    return result;
+};
 
 module.exports = maxProfit;
