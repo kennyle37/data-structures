5367c7b7aa00230ddea4c1489882bde207befd75
Kenny Le <kennyle37@gmail.com>
2019-09-28 11:42:20 +0900

    contains duplicate

--- LeetCode/exercises/LC217_containsDuplicate/index.js
+++ LeetCode/exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,15 @@
-const containsDuplicate = nums => {};
+var containsDuplicate = function(nums) {
+  let hash = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+      if (hash[nums[i]]) {
+          return true;
+      } else {
+          hash[nums[i]] = true;
+      }
+  }
+  
+  return false;
+};
 
 module.exports = containsDuplicate;
