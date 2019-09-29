94e798e5a7430ab514c701820b112c46d28659ee
Kenny Le <kennyle37@gmail.com>
2019-09-29 12:50:13 +0900

    product

--- LeetCode/exercises/LC238_productExceptSelf/index.js
+++ LeetCode/exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,19 @@
-const productExceptSelf = nums => {};
+var productExceptSelf = function(nums) {
+  if (nums.length <= 1) return [];
+  let dp = [];
+  dp[0] = 1;
+  let tmp = nums[nums.length-1];
+  
+  for (let i = 1; i < nums.length; i++) {
+      dp[i] = dp[i-1] * nums[i-1];
+  }
+  
+  for (let i = nums.length-2; i >= 0; i--) {
+      dp[i] = dp[i] * tmp;
+      tmp = tmp * nums[i];
+  }
+  
+  return dp;
+};
 
 module.exports = productExceptSelf;
