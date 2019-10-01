59cc268e903d032fe148527531f1214a96f40507
Kenny Le <kennyle37@gmail.com>
2019-10-01 17:44:51 +0900

    33

--- LeetCode/exercises/LC33_search/index.js
+++ LeetCode/exercises/LC33_search/index.js
@@ -1,3 +1,37 @@
-function search(nums, target) {}
+var search = function(nums, target) {
+  if (nums.length === 1 && nums[0] === target) return 0;
+  let l = 0;
+  let r = nums.length-1;
+  let m, lValue, rValue, mValue;
+  
+  while (l < r) {
+      m = Math.floor((l+r) / 2);
+      lValue = nums[l];
+      rValue = nums[r];
+      mValue = nums[m];
+      if (lValue === target) return l;
+      if (mValue === target) return m;
+      if (rValue === target) return r;
+      if (l+1 === r 
+          && nums[l] !== target 
+          && nums[r] !== target) break;
+      if (mValue > lValue) {
+          if (!(mValue >= target && lValue <= target)) {
+              l = m;
+          } else {
+              r = m;
+          }
+      }
+      if (mValue < rValue) {
+          if (!(mValue <= target && rValue >= target)) {
+              r = m;
+          } else {
+              l = m;
+          }
+      }
+
+  }
+  return -1;
+};
 
 module.exports = search;
